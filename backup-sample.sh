6767face7e9245fc04a2beea83c0ad0282529a58
Konstantin <KEIvanov@gmail.com>
2022-02-21 10:01:35 +0300

    v1.2.0 add --hard-links options and local copy ability (#1)
    * v1.2.0 add --hard-links option and local copy ability
    * add NOTE about --delete option
    * add backup-ignore-sample

--- backup-sample.sh
+++ backup-sample.sh
@@ -2,23 +2,60 @@
 
 #
 # https://github.com/x2es/simple-rsync-backup
-# v1.1.0
+# v1.2.0
 # 
 
-BACKUP_SERVER="x2es@backup.coolcab.org"
-CONTAINER="backup/container"
+# test your configuration and comment out DEBUG_ARGS after
+DEBUG_ARGS="--dry-run"
+
+# comment BACKUP_SERVER for local copy
+BACKUP_SERVER="user@backup.coolcab.org"
+
+# NOTE: non-absolute path points to user's home
+STORAGE="backup/storage"
+
+# by default CONTAINER is the name of the current directory
+# CONTAINER="custom-name"
+
 LOG_FILE="./backup.log"
 
-BACKUP_TARGET="$BACKUP_SERVER:~/$CONTAINER"
+# COMPRESS="--compress" # old compress
+COMPRESS="-zz" # works with rsync-3.1.2
+
+# prserve hard links
+# HARDLINKS="--hard-links"
+
+# Append your custom rsync args by setting CUSTOM_ARGS=
+# or by passing params to this script
+# CUSTOM_ARGS=""
+
+#                    #
+# --- CONFIG END --- #
+#                    #
+
+[[ -z "$CONTAINER" ]] && CONTAINER="$(basename $(pwd))"
+CONTAINER_PATH="$STORAGE/$CONTAINER"
+
+[[ ! -z "${@}" ]] && CUSTOM_ARGS="$CUSTOM_ARGS ${@}"
+
+[[ ! -z "$BACKUP_SERVER" ]]             && BACKUP_TARGET="$BACKUP_SERVER:"
+[[ "${CONTAINER_PATH:0:1}" != "/" ]]    && BACKUP_TARGET="${BACKUP_TARGET}~/"
+BACKUP_TARGET="${BACKUP_TARGET}${CONTAINER_PATH}/"
 
 EXCLUDE_LIST="./backup-ignore"
 EXCLUDE_ARG=""
 [[ -f $EXCLUDE_LIST ]] && EXCLUDE_ARG="--exclude-from=$EXCLUDE_LIST --delete-excluded"
 
 echo "Backup:"
+[[ ! -z "$DEBUG_ARGS" ]] && echo "  DEBUG: $DEBUG_ARGS"
+
+TARGET_NODE="$BACKUP_SERVER"
+[[ -z "$TARGET_NODE" ]] && TARGET_NODE="local"
+
 echo "        dir: `pwd`"                
-echo "         to: $BACKUP_SERVER"      
-echo "  container: $CONTAINER"
+echo "         to: $TARGET_NODE"
+echo "  container: $CONTAINER_PATH"
+[[ ! -z "$CUSTOM_ARGS" ]] && echo "custom args: $CUSTOM_ARGS"
 
 if [[ "$EXCLUDE_ARG" != "" ]]; then
   echo
@@ -31,13 +68,12 @@ echo
 echo "(press ENTER for continue / Ctrl+C for cancel)"
 read
 
-# COMPRESS="--compress" # old compress
-COMPRESS="-zz" # works with rsync-3.1.2
+COMMON_ARGS="$DEBUG_ARGS --archive $HARDLINKS $COMPRESS $CUSTOM_ARGS --stats --progress --itemize-changes"
 
-COMMON_ARGS="--dry-run --archive $COMPRESS --stats --progress --itemize-changes "
+rsync $COMMON_ARGS --recursive --log-file=$LOG_FILE $EXCLUDE_ARG --delete . $BACKUP_TARGET
 
-rsync $COMMON_ARGS --recursive --log-file=$LOG_FILE $EXCLUDE_ARG --delete   .           $BACKUP_TARGET
-rsync $COMMON_ARGS                                                          $LOG_FILE   $BACKUP_TARGET
+# Sync logfile
+[[ ! -z "$DEBUG_ARGS" ]] && echo "SKIP: $LOG_FILE didn't sync due DEBUG_ARGS=$DEBUG_ARGS" || rsync $COMMON_ARGS $LOG_FILE $BACKUP_TARGET
 
 echo
 echo "Done! (press ENTER to exit)"
